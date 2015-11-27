0fbb63658f5bd414326103b2bc4c5f97dd0c6734
sumorocker <macdonald.allan@live.com>
2015-11-27 21:49:35 +0000

    boilerplate.js updated

--- boilerplate.js
+++ boilerplate.js
@@ -1,59 +1,57 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: process.env.IP,
-  user: process.env.C9_USER,
-  database: 'addressbook'
+    user: 'sumorocker',
+    host: '127.0.0.1',
+    database: 'addressbook'
 });
 
-connection.query('select Account.id, AddressBook.name, Account.email from Account join AddressBook on Account.id=AddressBook.accountId', function(err, results) {
-  if(err){
-    console.log("error")
-  }
-  else {
-    
-  var newAccount = []
-  
-  results.forEach(function(oldAccount, index) {
-    var found;
-    var counter;
-    
-    newAccount.forEach(function(oldAccount, index) {
-      if (newAccount.id === oldAccount.id) {
-        found = true;
-        counter = index;
-      }
-    })
-    if (!found) {
-      var account = {
-        id: oldAccount.id,
-        email: oldAccount.email,
-        books: [oldAccount.name]
-      };
-      newAccount.push(account);
+connection.query('select Account.id as accountId, AddressBook.name as addressBookName, AddressBook.id as addressBookId, Account.email as accountEmail from Account join AddressBook on Account.id=AddressBook.accountId', function(err, results) {
+    if (err) {
+        console.log("error");
     }
     else {
-      newAccount[counter].books.push(oldAccount.name);
+        var ACCOUNTS = {}
+        var addressBooks = {}
+
+        results.forEach(function(row) {
+
+            var account = {
+                accountId: row.accountId,
+                accountEmail: row.accountEmail,
+                AddressBooks: []
+            };
+
+            var addressBook1 = {
+                addressBookId: row.addressBookId,
+                addressBookName: row.addressBookName
+            }
+
+            ACCOUNTS[account.accountId] = account;
+            addressBooks[addressBook1.addressBookId] = addressBook1;
+
+        })
+
+        var newAccount = []
+        results.forEach(function(row) {
+
+            var account = ACCOUNTS[row.accountId];
+            var addressBooks1 = addressBooks[row.addressBookId];
+
+            account.AddressBooks.push(addressBooks1.addressBookName);
+            if (newAccount.indexOf(account) === -1) {
+                newAccount.push(account)
+            }
+    
+        })
+        pushAccounts.forEach(function(account){
+       console.log(account.id+ '. '+ account.email);
+       account.books.forEach(function(book) {
+           console.log('    ' + book);
+       })
     }
-  })
-    newAccount.forEach(function(account){
-      console.log(account.id+ '. '+ account.email);
-      account.books.forEach(function(book) {
-          console.log('    ' + book);
-      })
-  })
-}
-  connection.end();
+
+   
 });
 
-// Here is an example usage:
-// rows.forEach(function(row) {
-//   console.log('#' + row.id + ': ' + row.email);
-// });
-// This code will output lines like:
-// #1: john@smith.com
-// #2: abc@def.com
-// #5: xx@yy.com
-
-// Note that IDs do not have to be contiguous. If we DELETE rows, there will be holes in the ID list. This is normal.
-// });
+connection.end();
